8ac64a15a89f4b0cd8ab37897fbfe679547e460d
logan <paczek.logan05@gmail.com>
2026-03-26 14:43:13 +0100

    feature.daily-input: create component for vehicule card

--- app/(main)/dashboard.tsx
+++ app/(main)/dashboard.tsx
@@ -2,8 +2,8 @@ import { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { useLocalSearchParams } from 'expo-router';
 import {  loadAuth } from '@/storage/authStorage';
-import { CarIcon } from '@/components/ui/icons';
 import { getVehiculeByVisiteurId } from '@/api/vehicules';
+import VehiculeCard from '@/components/dashboard/vehiculeCard';
 
 export default function ShowScreen() {
   const params = useLocalSearchParams<{
@@ -44,41 +44,7 @@ export default function ShowScreen() {
         <Text>Chargement...</Text>
       ) : (
         <>
-          <View style={styles.vehicleContainer}>
-            <View style={styles.vehicleHeader}>
-              <View style={styles.vehicleTitleContainer}>
-                <Text style={styles.vehicleHeaderTitle}>
-                  Véhicule Actuel
-                </Text>
-              </View>
-              <CarIcon fill="white"/>
-            </View>
-
-            <View style={styles.vehicleInfoContainer}>
-              <Text style={styles.vehicleModel}>
-                {vehicule?.marque ?? 'Non renseigné'}
-              </Text>
-              <Text style={styles.vehicleBrand}>
-                {vehicule?.modele ?? 'Non renseigné'}
-              </Text>
-            </View>
-
-            <View style={styles.vehicleInfoRow}>
-              <View style={styles.vehicleInfoPlateContainer}>
-                <Text style={styles.vehicleInfoPlate}>
-                  {vehicule?.immatriculation ?? 'Non renseigné'}
-                </Text>
-              </View>
-              <View style={styles.vehicleInfoTotalContainer}>
-                <Text style={styles.vehicleInfoTotal}>
-                  Total
-                </Text>
-                <Text style={styles.vehicleInfoTotalValue}>
-                  10000 km
-                </Text>
-              </View>
-            </View>
-          </View>
+          <VehiculeCard vehicule={vehicule} loading={loading} />
         </>
       )}
     </View>
@@ -92,77 +58,6 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     backgroundColor: '#FF45A0',
     paddingHorizontal: 16,
-  },
-
-  vehicleContainer: {
-    width: '80%',
-    backgroundColor: '#4F39F6',
-    borderRadius: 15,
-    padding: 16,
-    gap: 10,
-  },
-
-  // Header
-  vehicleHeader: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-  },
-  vehicleTitleContainer: {
-    paddingHorizontal: 10,
-    paddingVertical: 5,
-    borderRadius: 25,
-    backgroundColor: '#62748E',
-  },
-  vehicleHeaderTitle: {
-    fontSize: 12,
-    fontWeight: 'semibold',
-    color: 'white',
-  },
-
-  // Info
-  vehicleInfoContainer: {
-    flexDirection: 'column',
-    gap: 2,
-  },
-  vehicleModel: {
-    fontSize: 16,
-    fontWeight: 'bold',
-    color: "white",
-  },
-  vehicleBrand: {
-    fontSize: 14,
-    color: '#62748E',
-  },
-
-  // Info Row
-  vehicleInfoRow: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-  },
-  vehicleInfoPlateContainer: {
-    backgroundColor: '#4F1EBE',
-    paddingVertical: 5,
-    paddingHorizontal: 10,
-    borderRadius: 10,
-  },
-  vehicleInfoPlate: {
-    fontSize: 14,
-    color: "white",
-  },
-  vehicleInfoTotalContainer: {
-    alignItems: 'flex-end',
-  },
-  vehicleInfoTotal: {
-    fontSize: 14,
-    fontWeight: 'bold',
-    color: "white",
-  },
-  vehicleInfoTotalValue: {
-    fontSize: 14,
-    fontWeight: 'bold',
-    color: "white",
   },
 });
 
---
+++ components/dashboard/vehiculeCard.tsx
@@ -0,0 +1,126 @@
+import { StyleSheet, Text, View } from 'react-native';
+import { CarIcon } from '@/components/ui/icons';
+
+type Vehicule = {
+    marque?: string;
+    modele?: string;
+    immatriculation?: string;
+  };
+  type VehiculeCardProps = {
+    vehicule: Vehicule | null;
+    loading?: boolean;
+  };
+
+export default function VehiculeCard({ vehicule, loading }: VehiculeCardProps) {
+
+  return (
+    <View style={styles.vehicleContainer}>
+        <View style={styles.vehicleHeader}>
+            <View style={styles.vehicleTitleContainer}>
+            <Text style={styles.vehicleHeaderTitle}>
+                Véhicule Actuel
+            </Text>
+            </View>
+            <CarIcon fill="white"/>
+        </View>
+
+        <View style={styles.vehicleInfoContainer}>
+            <Text style={styles.vehicleModel}>
+            {vehicule?.marque ?? 'Non renseigné'}
+            </Text>
+            <Text style={styles.vehicleBrand}>
+            {vehicule?.modele ?? 'Non renseigné'}
+            </Text>
+        </View>
+
+        <View style={styles.vehicleInfoRow}>
+            <View style={styles.vehicleInfoPlateContainer}>
+            <Text style={styles.vehicleInfoPlate}>
+                {vehicule?.immatriculation ?? 'Non renseigné'}
+            </Text>
+            </View>
+            <View style={styles.vehicleInfoTotalContainer}>
+            <Text style={styles.vehicleInfoTotal}>
+                Total
+            </Text>
+            <Text style={styles.vehicleInfoTotalValue}>
+                10000 km
+            </Text>
+            </View>
+        </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  vehicleContainer: {
+    width: '80%',
+    backgroundColor: '#4F39F6',
+    borderRadius: 15,
+    padding: 16,
+    gap: 10,
+  },
+
+  // Header
+  vehicleHeader: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  vehicleTitleContainer: {
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    borderRadius: 25,
+    backgroundColor: '#62748E',
+  },
+  vehicleHeaderTitle: {
+    fontSize: 12,
+    fontWeight: 'semibold',
+    color: 'white',
+  },
+
+  // Info
+  vehicleInfoContainer: {
+    flexDirection: 'column',
+    gap: 2,
+  },
+  vehicleModel: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: "white",
+  },
+  vehicleBrand: {
+    fontSize: 14,
+    color: '#62748E',
+  },
+
+  // Info Row
+  vehicleInfoRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  vehicleInfoPlateContainer: {
+    backgroundColor: '#4F1EBE',
+    paddingVertical: 5,
+    paddingHorizontal: 10,
+    borderRadius: 10,
+  },
+  vehicleInfoPlate: {
+    fontSize: 14,
+    color: "white",
+  },
+  vehicleInfoTotalContainer: {
+    alignItems: 'flex-end',
+  },
+  vehicleInfoTotal: {
+    fontSize: 14,
+    fontWeight: 'bold',
+    color: "white",
+  },
+  vehicleInfoTotalValue: {
+    fontSize: 14,
+    fontWeight: 'bold',
+    color: "white",
+  },
+});
