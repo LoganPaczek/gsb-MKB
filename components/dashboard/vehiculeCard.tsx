95db75038f4adb5e8d4054ed35ffea253444fd5a
logan <paczek.logan05@gmail.com>
2026-03-26 16:58:02 +0100

    feature/dashboard: improve design

--- components/dashboard/vehiculeCard.tsx
+++ components/dashboard/vehiculeCard.tsx
@@ -22,7 +22,7 @@ export default function VehiculeCard({ vehicule, loading, totalKmWithVehicule }:
                 Véhicule Actuel
             </Text>
             </View>
-            <CarIcon fill="white"/>
+            <CarIcon fill="#E0E7FF"/>
         </View>
 
         <View style={styles.vehicleInfoContainer}>
@@ -92,7 +92,7 @@ const styles = StyleSheet.create({
   },
   vehicleBrand: {
     fontSize: 14,
-    color: '#62748E',
+    color: '#E0E7FF',
   },
 
   // Info Row
